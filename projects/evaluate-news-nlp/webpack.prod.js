0e0bb144e3c3f13b817d95cb12e4d50c8c534c17
Aysheh Alshishani <ayshehalshishani@AyshehsLaptop2.attlocal.net>
2023-07-11 16:35:20 -0400

    Add Service Workers

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,8 +1,10 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const WorkboxPlugin = require('workbox-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,6 +13,9 @@ module.exports = {
         libraryTarget: 'var',
         library: 'Client'
     },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
     module: {
         rules: [
             {
@@ -19,9 +24,10 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-                test: /.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            
         ]
     },
     plugins: [
@@ -29,7 +35,9 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
         new WorkboxPlugin.GenerateSW(),
+
 
     ]
 }
